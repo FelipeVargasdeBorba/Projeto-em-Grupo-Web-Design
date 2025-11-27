2202a3c1cede78674fe9859b80af170a9a7ab9eb
FelipeVargasdeBorba <felipevargasvb@gmail.com>
2025-11-27 13:25:15 -0300

    Adição: Carrossel de Banner, Carrinho, Logo do site, imagens de produtos e ajustes

---
+++ js/cart.js
@@ -0,0 +1,11 @@
+let cartItemsData = []; /* Lista para que consiga adicionar produtos no carrinho*/
+
+function openCart() {
+    document.getElementById('cartDrawer').classList.add('is-open');
+}
+
+function closeCart() {
+    document.getElementById('cartDrawer').classList.remove('is-open');
+}
+
+/* código ainda não funcional */
